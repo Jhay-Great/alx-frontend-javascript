147d0a7d2b7642bf961bca317bed6bf51569ec98
Jhay-Great <alemonai.jenef@gmail.com>
2024-03-13 02:57:01 +0000

    task 3 correction

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,5 @@
-import getListStudents from './0-get_list_students';
-
 function getStudentIdSum(array) {
   return array.reduce((total, person) => total + person.id, 0);
 }
 
-console.log(getStudentIdSum(getListStudents()));
+export default getStudentIdSum;
